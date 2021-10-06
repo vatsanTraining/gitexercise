f0f58369efcf98ad8d47e70dd9d1ea86195c2901
srivatsan <vatsan_k@hotmail.com>
2021-10-06 21:55:38 +0530

    First Example

---
+++ firstExample/src/com/example/demo/Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Greeting grtObj = new Greeting();
+		
+		System.out.println(grtObj.getMessage());
+		
+	}
+
+}
---
+++ firstExample/src/com/example/demo/Greeting.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class Greeting {
+
+	
+	public String getMessage() {
+		
+		return "Hello World";
+	}
+}
